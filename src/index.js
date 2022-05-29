8848cc69b1add8438c10b2d6ee2d8eb39a85ecd9
angelo28b <101931022+angelo28b@users.noreply.github.com>
2022-05-29 18:20:32 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Head from './Head';
+import Tex from './Tex'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+ <Head />
+ <Tex />
+ </>
 );
 
 // If you want to start measuring performance in your app, pass a function
